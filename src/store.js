b8aea93884a69ef78d0d67730b0e3cbbd6c9eaec
Jessica <jesscara.993@gmail.com>
2022-12-21 15:01:04 +0100

    added icon element to links array

--- src/store.js
+++ src/store.js
@@ -6,31 +6,37 @@ export const store = reactive({
       text: 'Home',
       url: '#',
       current: true,
+      icon: true,
     },
     {
       text: 'Shop',
       url: '#',
       current: false,
+      icon: true,
     },
     {
       text: 'About',
       url: '#',
       current: false,
+      icon: false,
     },
     {
       text: 'Blog',
       url: '#',
       current: false,
+      icon: false,
     },
     {
       text: 'Contact',
       url: '#',
       current: false,
+      icon: false,
     },
     {
       text: 'Shop by brand',
       url: '#',
       current: false,
+      icon: true,
     },
   ]
 })
