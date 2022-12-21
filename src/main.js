a36d41839866f7449c598de786fefdc82fe196ea
Jessica <jesscara.993@gmail.com>
2022-12-21 14:58:14 +0100

    changed chevron icon to angle

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ import "@fontsource/work-sans"
 // Fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faDollarSign, faTruck, faCheck, faMagnifyingGlass, faChevronDown, faBagShopping } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign, faTruck, faCheck, faMagnifyingGlass, faAngleDown, faBagShopping } from '@fortawesome/free-solid-svg-icons'
 import { faCreditCard, faMap, faClock, faUser } from '@fortawesome/free-regular-svg-icons'
 import { faFacebookF, faInstagram, faTwitter, faYoutube, } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faFacebookF, faTwitter, faInstagram, faYoutube, faTruck, faMap, faDollarSign, faCreditCard, faClock, faCheck, faMagnifyingGlass, faChevronDown, faUser, faBagShopping)
+library.add(faFacebookF, faTwitter, faInstagram, faYoutube, faTruck, faMap, faDollarSign, faCreditCard, faClock, faCheck, faMagnifyingGlass, faAngleDown, faUser, faBagShopping)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
