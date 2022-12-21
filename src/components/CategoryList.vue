4d9cc14fe89e3d7c93d66135ddc65cb2f5c4e7eb
Jessica <jesscara.993@gmail.com>
2022-12-21 21:38:59 +0100

    created component with array for category

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        {
+          title: 'Bed (1)',
+          image: '/img/product-9-300x300.jpg',
+        },
+        {
+          title: 'Food (6)',
+          image: '/img/product-3-300x300.jpg',
+        },
+        {
+          title: 'Toys (6)',
+          image: '/img/product-2-300x300.jpg',
+        },
+        {
+          title: 'Transport (6)',
+          image: '/img/product-4-300x300.jpg',
+        }
+      ]
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card" v-for="category in categories" :key="id">
+    <img :src="category.image" :alt="category.title">
+    <span>{{ category.title }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.card {
+  width: 220px;
+  margin-bottom: 80px;
+  text-align: center;
+
+  img {
+    width: 100%;
+    margin-bottom: 10px;
+  }
+}
+</style>
