34c97397593ccf59e7f4e046f11542a503f1cbff
Jessica <jesscara.993@gmail.com>
2022-12-22 18:37:07 +0100

    hover on articles done

--- src/components/BlogSection.vue
+++ src/components/BlogSection.vue
@@ -4,21 +4,25 @@ export default {
     return {
       articles: [
         {
+          id: '1',
           image: '/img/blog-1-200x200.jpg',
           title: 'How to "loose leash walk" your dog',
           date: 'April 21st, 2020'
         },
         {
+          id: '2',
           image: '/img/blog-2-200x200.jpg',
           title: 'Stop cats from scratching furniture',
           date: 'April 19th, 2020'
         },
         {
+          id: '3',
           image: '/img/blog-3-200x200.jpg',
           title: 'Where to buy the best pet food',
           date: 'April 18th, 2020'
         },
         {
+          id: '4',
           image: '/img/blog-4-200x200.jpg',
           title: 'Proper care for your pet toys',
           date: 'April 17th, 2020'
@@ -35,12 +39,19 @@ export default {
       <h2>Get the best tips &amp; tricks</h2>
       <h5>Recent articles</h5>
 
-      <div class="articles" v-for="article in articles" :key="id">
+      <div class="articles" v-for="article in articles" :key="id" @mouseover="article.isHovered = true"
+        @mouseout="article.isHovered = false">
         <a href="#">
           <img :src="article.image" :alt="article.title">
           <h3>{{ article.title }}</h3>
           <span>{{ article.date }}</span>
         </a>
+
+        <!-- View title on hover -->
+        <div class="check" v-show="article.isHovered">
+          <span>{{ article.title }}</span>
+          <span>Tips &amp; tricks</span>
+        </div>
       </div>
 
       <div class="btn">
@@ -72,7 +83,23 @@ export default {
   }
 
   .articles {
+    position: relative;
     width: 200px;
+
+    .check {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 75px;
+      padding: 10px;
+      text-align: center;
+
+      span {
+        color: $white;
+        font-size: 1rem;
+      }
+    }
 
     h3 {
       padding: 10px 10px 0;
