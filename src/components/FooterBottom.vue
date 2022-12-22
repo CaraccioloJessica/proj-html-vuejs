56b623b3a5f3a0cb0bb3f256eab88fb84e9885b0
Jessica <jesscara.993@gmail.com>
2022-12-22 16:59:24 +0100

    created and styled, nav, brand icon and copyright

---
+++ src/components/FooterBottom.vue
@@ -0,0 +1,85 @@
+<script>
+import { store } from '../store.js'
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  <section id="kelp">
+    <nav>
+      <ul>
+        <!-- Cicla nell'array dei footerLinks per la navigazione -->
+        <li v-for="link in store.footerLinks" :key="id">
+          <a :class="{ active: link.current }" :href="link.url">{{ link.text }}
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+    <div id="brand">
+      <font-awesome-icon icon="fa-brands fa-facebook-f" />
+      <font-awesome-icon icon="fa-brands fa-twitter" />
+      <font-awesome-icon icon="fa-brands fa-instagram" />
+      <font-awesome-icon icon="fa-brands fa-youtube" />
+    </div>
+
+    <div id="copyright">
+      <span>&copy; Copyright 2012 - 2020 &vert; Avada Theme by ThemeFusion &vert; All Rights Reserved &vert; Powered by
+        Wordpress</span>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+#kelp {
+  background-color: $kelp;
+  color: $white;
+
+  ul {
+    display: flex;
+    justify-content: center;
+    padding: 30px 0;
+
+    li {
+      color: $grey;
+      font-size: 0.75rem;
+      margin: 0 10px;
+
+      a {
+
+        &:hover,
+        &.active {
+          color: $white;
+        }
+      }
+    }
+  }
+
+  #brand {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 80px;
+
+    svg {
+      margin: 0 10px;
+    }
+  }
+
+  #copyright {
+    font-size: 0.6rem;
+    background-color: black;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
